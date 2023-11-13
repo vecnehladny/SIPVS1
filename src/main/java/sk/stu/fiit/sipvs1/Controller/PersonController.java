e9097adc4b784bc3533820ecda52e1f7a7f2a847
Viktor Klima <viktorklima@yahoo.com>
2023-11-13 21:02:47 +0100

    add timestamp to xml signature

--- src/main/java/sk/stu/fiit/sipvs1/Controller/PersonController.java
+++ src/main/java/sk/stu/fiit/sipvs1/Controller/PersonController.java
@@ -2,34 +2,19 @@ package sk.stu.fiit.sipvs1.Controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import sk.stu.fiit.sipvs1.Model.Child;
 import sk.stu.fiit.sipvs1.Model.Person;
 import sk.stu.fiit.sipvs1.Service.PersonService;
-import sk.stu.fiit.sipvs1.wrapper.Alert;
-import sk.stu.fiit.sipvs1.wrapper.XSDValidationResult;
 
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.*;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Controller
@@ -42,9 +27,9 @@ public class PersonController {
 
     @GetMapping("/")
     public String showPersonForm(Person person, Model model) {
-        model.addAttribute("xsdFile", getSampleXsd());
-        model.addAttribute("xslFile", getSampleXsl());
-        model.addAttribute("pdfFile", getSamplePdf());
+        model.addAttribute("xsdFile", personService.getSampleXsd());
+        model.addAttribute("xslFile", personService.getSampleXsl());
+        model.addAttribute("pdfFile", personService.getSamplePdf());
         return "person-form";
     }
 
@@ -61,116 +46,21 @@ public class PersonController {
     @GetMapping("/process/save-to-xml")
     @ResponseBody
     public void saveToXml(Person person, HttpServletResponse response) {
-        File personAsXmlFile = personService.getPersonAsXmlFile(person);
-        if(null != personAsXmlFile) {
-            try {
-                String mimeType = URLConnection.guessContentTypeFromName(personAsXmlFile.getName());
-                String contentDisposition = String.format("attachment; filename=%s", personAsXmlFile.getName());
-                int fileSize = Long.valueOf(personAsXmlFile.length())
-                                   .intValue();
-                response.setContentType(mimeType);
-                response.setHeader("Content-Disposition", contentDisposition);
-                response.setContentLength(fileSize);
-                OutputStream out = response.getOutputStream();
-                Files.copy(personAsXmlFile.toPath(), out);
-                out.flush();
-            } catch (IOException e) {
-                LOGGER.log(Level.SEVERE, "Failed send a file", e);
-            }
-        }
+        personService.saveFormToXml(person, response);
     }
 
     @PostMapping("/process/validate-against-xsd")
     public String validateAgainstXsd(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-        XSDValidationResult validationResult = personService.validateXmlAgainstXsd(file);
-        String message;
-        String type;
-        Exception exception = null;
-        if (validationResult.isValid()) {
-            message = "Validation successful for file ".concat(file.getOriginalFilename());
-            type = "success";
-        } else {
-            message = "Validation failed for file ".concat(file.getOriginalFilename());
-            type = "danger";
-            exception = validationResult.getException();
-        }
-        redirectAttributes.addFlashAttribute("alerts",
-                           Collections.singletonList(Alert.builder()
-                                                          .message(message)
-                                                          .type(type)
-                                                          .exception(exception)
-                                                          .build()));
-        return "redirect:/";
+        return personService.validateXmlAgainstXsd(file, redirectAttributes);
     }
 
     @PostMapping("/process/transform-xml-to-html")
     public void transformXmlToHtml(@RequestParam("file") MultipartFile file, HttpServletResponse response) {
-        try {
-            File xslStylesheet = new File("src/main/resources/sample.xsl");
-            TransformerFactory factory = TransformerFactory.newInstance();
-            Transformer transformer = factory.newTransformer(new StreamSource(xslStylesheet));
-            File htmlFile = new File("index.html");
-            FileWriter fileWriter = new FileWriter(htmlFile);
-            transformer.transform(new StreamSource(file.getInputStream()), new StreamResult(fileWriter));
-            fileWriter.close();
-            String mimeType = URLConnection.guessContentTypeFromName(htmlFile.getName());
-            String contentDisposition = String.format("attachment; filename=%s", htmlFile.getName());
-            int fileSize = Long.valueOf(htmlFile.length())
-                               .intValue();
-            response.setContentType(mimeType);
-            response.setHeader("Content-Disposition", contentDisposition);
-            response.setContentLength(fileSize);
-            OutputStream out = response.getOutputStream();
-            Files.copy(htmlFile.toPath(), out);
-            out.flush();
-        } catch (IOException | TransformerException e) {
-            LOGGER.log(Level.SEVERE, "Failed to send a file", e);
-        }
+        personService.transformXmlToHtml(file, response);
     }
 
-    private Person getSamplePerson() {
-        return Person.builder()
-                     .personID("1")
-                     .name("Viktor")
-                     .age(12)
-                     .email("email@gmail.com")
-                     .birthDate(LocalDate.now())
-                     .children(Arrays.asList(Child.builder()
-                                                  .firstName("Decko")
-                                                  .lastName("1")
-                                                  .build(),
-                                             Child.builder()
-                                                  .firstName("Decko")
-                                                  .lastName("2")
-                                                  .build()))
-                     .build();
-    }
-
-    private String getSamplePdf() {
-        try {
-            byte[] file = Files.readAllBytes(Path.of("src/main/resources/sample.pdf"));
-            byte[] encoded = Base64.encodeBase64(file, false);
-            return new String(encoded, StandardCharsets.US_ASCII);
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Failed to read sample pdf file");
-        }
-        return null;
-    }
-
-    private String getSampleXsl() {
-        return readFileToString("src/main/resources/sample.xsl");
-    }
-
-    private String getSampleXsd() {
-        return readFileToString("src/main/resources/sample.xsd");
-    }
-
-    private String readFileToString(String path) {
-        try {
-            return Files.readString(Path.of(path));
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Failed to read file " + path);
-        }
-        return null;
+    @PostMapping("/process/timestamp-signature")
+    public void addTimestampToSignature(@RequestParam("file") MultipartFile file, HttpServletResponse response) {
+        personService.addTimestampToSignature(file, response);
     }
 }
--- src/main/java/sk/stu/fiit/sipvs1/Service/PersonService.java
+++ src/main/java/sk/stu/fiit/sipvs1/Service/PersonService.java
@@ -3,19 +3,35 @@ package sk.stu.fiit.sipvs1.Service;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.tsp.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.xml.sax.SAXException;
 import sk.stu.fiit.sipvs1.Model.Person;
+import sk.stu.fiit.sipvs1.wrapper.Alert;
 import sk.stu.fiit.sipvs1.wrapper.XSDValidationResult;
 
 import javax.xml.XMLConstants;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,6 +40,31 @@ public class PersonService {
 
     public static final Logger LOGGER = Logger.getLogger("PersonService");
     public static final String DEFAULT_FILE_NAME = "sample_file.xml";
+    public static final String SIGNATURE_START = "<ds:SignatureValue Id=\"signatureIdSignatureValue\" xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\" xmlns:xzep=\"http://www.ditec.sk/ep/signature_formats/xades_zep/v2.0\">";
+    public static final String SIGNATURE_END = "</ds:SignatureValue>";
+
+    @Value("${tsa.url}")
+    String tsaUrl;
+
+    public void saveFormToXml(Person person, HttpServletResponse response) {
+        File personAsXmlFile = getPersonAsXmlFile(person);
+        if(null != personAsXmlFile) {
+            try {
+                String mimeType = URLConnection.guessContentTypeFromName(personAsXmlFile.getName());
+                String contentDisposition = String.format("attachment; filename=%s", personAsXmlFile.getName());
+                int fileSize = Long.valueOf(personAsXmlFile.length())
+                                   .intValue();
+                response.setContentType(mimeType);
+                response.setHeader("Content-Disposition", contentDisposition);
+                response.setContentLength(fileSize);
+                OutputStream out = response.getOutputStream();
+                Files.copy(personAsXmlFile.toPath(), out);
+                out.flush();
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, "Failed send a file", e);
+            }
+        }
+    }
 
     public File getPersonAsXmlFile(Person person) {
         String fileName = null != person.getName() ? getFileNameFromName(person) : DEFAULT_FILE_NAME;
@@ -34,6 +75,28 @@ public class PersonService {
             LOGGER.log(Level.SEVERE, "Failed to create a file", e);
         }
         return null;
+    }
+
+    public String validateXmlAgainstXsd(MultipartFile file, RedirectAttributes redirectAttributes) {
+        XSDValidationResult validationResult = validateXmlAgainstXsd(file);
+        String message;
+        String type;
+        Exception exception = null;
+        if (validationResult.isValid()) {
+            message = "Validation successful for file ".concat(file.getOriginalFilename());
+            type = "success";
+        } else {
+            message = "Validation failed for file ".concat(file.getOriginalFilename());
+            type = "danger";
+            exception = validationResult.getException();
+        }
+        redirectAttributes.addFlashAttribute("alerts",
+                                             Collections.singletonList(Alert.builder()
+                                                                            .message(message)
+                                                                            .type(type)
+                                                                            .exception(exception)
+                                                                            .build()));
+        return "redirect:/";
     }
 
     public XSDValidationResult validateXmlAgainstXsd(MultipartFile file) {
@@ -57,6 +120,108 @@ public class PersonService {
                                   .build();
     }
 
+    public void transformXmlToHtml(MultipartFile file, HttpServletResponse response) {
+        try {
+            File xslStylesheet = new File("src/main/resources/sample.xsl");
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer(new StreamSource(xslStylesheet));
+            File htmlFile = new File("index.html");
+            FileWriter fileWriter = new FileWriter(htmlFile);
+            transformer.transform(new StreamSource(file.getInputStream()), new StreamResult(fileWriter));
+            fileWriter.close();
+            String mimeType = URLConnection.guessContentTypeFromName(htmlFile.getName());
+            String contentDisposition = String.format("attachment; filename=%s", htmlFile.getName());
+            int fileSize = Long.valueOf(htmlFile.length())
+                               .intValue();
+            response.setContentType(mimeType);
+            response.setHeader("Content-Disposition", contentDisposition);
+            response.setContentLength(fileSize);
+            OutputStream out = response.getOutputStream();
+            Files.copy(htmlFile.toPath(), out);
+            out.flush();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Failed to send a file", e);
+        }
+    }
+
+    public void addTimestampToSignature(MultipartFile file, HttpServletResponse response) {
+        try {
+            String xmlSignature = new String(file.getInputStream().readAllBytes());
+            byte[] timestampToken = getTimestampFromTSA(xmlSignature);
+            String unsignedProperties = "<xades:UnsignedProperties><xades:UnsignedSignatureProperties><xades:SignatureTimeStamp Id=\"signatureIdSignatureTimeStamp\"><xades:EncapsulatedTimeStamp>$TIMESTAMP$</xades:EncapsulatedTimeStamp></xades:SignatureTimeStamp></xades:UnsignedSignatureProperties></xades:UnsignedProperties>";
+            unsignedProperties = unsignedProperties.replace("$TIMESTAMP$", Base64.encodeBase64String(timestampToken));
+            xmlSignature = xmlSignature.replace("</xades:SignedProperties>", "</xades:SignedProperties>".concat(unsignedProperties));
+
+            File timestampedSignatureFile = new File("xades_t_signature.xml");
+            if (null != timestampedSignatureFile) {
+                String mimeType = URLConnection.guessContentTypeFromName(timestampedSignatureFile.getName());
+                String contentDisposition = String.format("attachment; filename=%s",
+                                                          timestampedSignatureFile.getName());
+                int fileSize = Long.valueOf(timestampedSignatureFile.length())
+                                   .intValue();
+                response.setContentType(mimeType);
+                response.setHeader("Content-Disposition", contentDisposition);
+                response.setContentLength(fileSize);
+                OutputStream out = response.getOutputStream();
+                FileWriter fw = new FileWriter(timestampedSignatureFile.getAbsoluteFile());
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(xmlSignature);
+                bw.close();
+                Files.copy(timestampedSignatureFile.toPath(), out);
+                out.flush();
+            }
+
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error timestamping the signature", e);
+        }
+    }
+
+
+    private byte[] getTimestampFromTSA(String signatureFile) throws IOException, TSPException {
+        TimeStampRequestGenerator tsReqGen = new TimeStampRequestGenerator();
+        tsReqGen.setCertReq(true);
+        TimeStampRequest tsReq = tsReqGen.generate(TSPAlgorithms.SHA256, calculateMessageDigest(signatureFile));
+        byte[] requestBytes = tsReq.getEncoded();
+        byte[] responseBytes = sendTimeStampRequest(tsaUrl, requestBytes);
+        TimeStampResponse tsResp = new TimeStampResponse(new ByteArrayInputStream(responseBytes));
+        tsResp.validate(tsReq);
+        return tsResp.getTimeStampToken().getEncoded();
+    }
+
+    private byte[] sendTimeStampRequest(String tsaUrl, byte[] requestBytes) throws IOException {
+        URL url = new URL(tsaUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/timestamp-query");
+        connection.setDoOutput(true);
+
+        try (java.io.OutputStream outputStream = connection.getOutputStream()) {
+            outputStream.write(requestBytes);
+        }
+
+        try (java.io.InputStream inputStream = connection.getInputStream()) {
+            return inputStream.readAllBytes();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    private byte[] calculateMessageDigest(String signatureFile) {
+        String toStamp = signatureFile;
+        toStamp = toStamp.substring(toStamp.indexOf(SIGNATURE_START) + SIGNATURE_START.length());
+        toStamp = toStamp.substring(0, toStamp.indexOf(SIGNATURE_END));
+
+        SHA256Digest md = new SHA256Digest();
+        byte[] bytes = toStamp.getBytes();
+        String fileString = new String(bytes);
+        byte[] dataBytes = fileString.getBytes();
+        int nread = dataBytes.length;
+        md.update(dataBytes, 0, nread);
+        byte[] result = new byte[32];
+        md.doFinal(result, 0);
+        return result;
+    }
+
     private XmlMapper xmlMapper() {
         return XmlMapper.builder()
                         .enable(SerializationFeature.INDENT_OUTPUT)
@@ -73,5 +238,31 @@ public class PersonService {
                      .concat(".xml");
     }
 
+    public String getSamplePdf() {
+        try {
+            byte[] file = Files.readAllBytes(Path.of("src/main/resources/sample.pdf"));
+            byte[] encoded = Base64.encodeBase64(file, false);
+            return new String(encoded, StandardCharsets.US_ASCII);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Failed to read sample pdf file");
+        }
+        return null;
+    }
 
+    public String getSampleXsl() {
+        return readFileToString("src/main/resources/sample.xsl");
+    }
+
+    public String getSampleXsd() {
+        return readFileToString("src/main/resources/sample.xsd");
+    }
+
+    private String readFileToString(String path) {
+        try {
+            return Files.readString(Path.of(path));
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Failed to read file " + path);
+        }
+        return null;
+    }
 }
